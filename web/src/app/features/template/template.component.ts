22c0f8a194f273b8666824d5cde78241792aec60
Komiljon <komiljonnajmitdinov99@gmail.com>
2024-05-15 11:25:32 +0500

    some changes

--- web/src/app/features/template/template.component.ts
+++ web/src/app/features/template/template.component.ts
@@ -1,15 +1,109 @@
-import { Component } from '@angular/core';
-import {EditorModule} from "@tinymce/tinymce-angular";
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { SidebarModule } from 'primeng/sidebar';
+import { SliderModule } from 'primeng/slider';
+import { Table, TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { TooltipModule } from 'primeng/tooltip';
+import { Customer, DocTemplate, Representative } from 'src/app/demo/api/customer';
+import { Product } from 'src/app/demo/api/product';
+import { OverlaysDemoRoutingModule } from 'src/app/demo/components/uikit/overlays/overlaysdemo-routing.module';
+import { TableDemoRoutingModule } from 'src/app/demo/components/uikit/table/tabledemo-routing.module';
+import { CustomerService } from 'src/app/demo/service/customer.service';
+import { ProductService } from 'src/app/demo/service/product.service';
 
 @Component({
-  selector: 'app-template',
-  standalone: true,
+    selector: 'app-template',
+    standalone: true,
     imports: [
-        EditorModule
+        EditorModule,
+        TableDemoRoutingModule,
+        FormsModule,
+        TableModule,
+        RatingModule,
+        ButtonModule,
+        SliderModule,
+        InputTextModule,
+        ToggleButtonModule,
+        RippleModule,
+        MultiSelectModule,
+        DropdownModule,
+        ProgressBarModule,
+        ToastModule,
+        OverlaysDemoRoutingModule,
+        ToastModule,
+        DialogModule,
+        FormsModule,
+        TooltipModule,
+        InputTextModule,
+        ButtonModule,
+        OverlayPanelModule,
+        TableModule,
+        ConfirmDialogModule,
+        SidebarModule,
+        RippleModule,
+        ConfirmPopupModule,
     ],
-  templateUrl: './template.component.html',
-  styleUrl: './template.component.scss'
+    providers: [MessageService, ConfirmationService],
+    templateUrl: './template.component.html',
+    styleUrl: './template.component.scss',
 })
 export class TemplateComponent {
+    display: boolean = false;
+    customers3: DocTemplate[] = [];
 
+  
+
+    loading: boolean = true;
+
+    @ViewChild('filter') filter!: ElementRef;
+
+    constructor(
+        private customerService: CustomerService,
+        private productService: ProductService
+    ) {}
+
+    ngOnInit() {
+        this.customerService.getDocTemplates().then((customers) => {
+
+            this.customers3 = customers;
+        });
+    }
+
+
+
+    expandAll() {}
+
+    formatCurrency(value: number) {
+        return value.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        });
+    }
+
+    onGlobalFilter(table: Table, event: Event) {
+        table.filterGlobal(
+            (event.target as HTMLInputElement).value,
+            'contains'
+        );
+    }
+
+    clear(table: Table) {
+        table.clear();
+        this.filter.nativeElement.value = '';
+    }
 }
